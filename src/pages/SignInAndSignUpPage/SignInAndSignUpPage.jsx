a8251fbbfe17edf522d943b9d35c8025802149e1
Naveen Singh Adhikari <meetnsa1@gmail.com>
2019-09-27 09:06:54 +0530

    Custom forms and Button added

---
+++ src/pages/SignInAndSignUpPage/SignInAndSignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SignInAndSignUpPage.style.scss";
+import SignIn from "../../componets/sign-in/sign-in";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
